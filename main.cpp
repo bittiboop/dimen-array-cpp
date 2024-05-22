95c4db593d5092ac98bfddcd9c739075b10198a2
bittiboop <sstrelkova15@gmail.com>
2024-05-22 20:16:53 +0300

    added first ver of code

--- main.cpp
+++ main.cpp
@@ -4,78 +4,46 @@ using namespace std;
 
 int main()
 {
-	const int cols = 100;
-	const int rows = 100;
-	int matrix[rows][cols];
-	int current_cols = 0, current_rows = 0;
+	const int cols = 10;
+	const int cols_2 = 5;
+	const int rows = 5;
+	const int rows_2 = 5;
+	int matrix_1[rows][cols];
+	int matrix_2[rows_2][cols_2];
 	srand(time(0));
-	do {
-		cout << "Enter the number of columns: ";
-		cin >> current_cols;
-		cout << "Enter the number of rows: ";
-		cin >> current_rows;
-	} while (current_cols > cols || current_rows > rows || current_rows < 0 || current_cols < 0);
 
-	cout << "Matrix: " << endl;
-	for (int i = 0; i < current_rows; i++)
+	cout << "First matrix: " << endl;
+	for (int i = 0; i < rows; i++)
 	{
-		for (int j = 0; j < current_cols; j++)
+		for (int j = 0; j < cols; j++)
 		{
-			matrix[i][j] = rand() % 10;
+			matrix_1[i][j] = rand() % 50;
 		}
 	}
-	for (int i = 0; i < current_rows; i++)
+	for (int i = 0; i < rows; i++)
 	{
-		for (int j = 0; j < current_cols; j++) {
-			cout << matrix[i][j] << " ";
+		for (int j = 0; j < cols; j++) {
+			cout << matrix_1[i][j] << " ";
 		}
 		cout << endl;
 	}cout << endl << endl;
-	cout << "Sum of elements in the matrix: " << endl;
-	int sum = 0;
-	for (size_t i = 0; i < current_rows; i++)
+	
+	cout << "Second matrix: " << endl;
+	for (int i = 0; i < rows_2; i++)
 	{
-		for (size_t j = 0; j < current_cols; j++)
-		{
-			sum += matrix[i][j];
-		}
+		matrix_2[0][i] = matrix_1[0][i] + matrix_1[1][i];
 	}
-	cout << sum << endl;
-	cout << "Average: ";
-	int avg = 0;
-	for (size_t i = 0; i < current_rows; i++)
+	for (int i = 0; i < cols_2; i++)
 	{
-		for (size_t j = 0; j < current_cols; j++)
-		{
-			avg = sum / 2;
-		}
+		matrix_2[i][0] = matrix_1[i][0] + matrix_1[i][1];
 	}
-	cout << avg << endl;
-	cout << "Max: ";
-	int max = matrix[0][0];
-	for (size_t i = 0; i < current_rows; i++)
+	for (int i = 0; i < rows_2; i++)
 	{
-		for (size_t j = 0; j < current_cols; j++)
-		{
-			if (matrix[i][j] > max)
-			{
-				max = matrix[i][j];
-			}
+		for (int j = 0; j < cols_2; j++) {
+			cout << matrix_2[i][j] << " ";
 		}
+		cout << endl;
 	}
-	cout << max << endl;
-	cout << "Min: ";
-	int min = matrix[0][0];
-	for (size_t i = 0; i < current_rows; i++)
-	{
-		for (size_t j = 0; j < current_cols; j++)
-		{
-			if (matrix[i][j] < min)
-			{
-				min = matrix[i][j];
-			}
-		}
-	}
-	cout << min << endl;
+	
 	return 0;
 }
